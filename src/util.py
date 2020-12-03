be3c5f388074f356dfa3c534f4f8e36ec0bd59b8
Mohammed Alqumairi <moh.alqumairi@gmail.com>
2020-12-03 17:17:43 +0000

    NN 1

--- src/util.py
+++ src/util.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold
+from sklearn import metrics
 
 
 # A function that takes the data in, and cleans it
@@ -99,7 +100,7 @@ def confusion_plot(y_test, y_predictions, target_classes=police_actions_simple, 
 
 
 # A function that takes in a model, and a dataframe, and trains the model using K-fold
-def k_fold_train(model, data, folds=5):
+def k_fold_train(model, data, folds=5, is_NN=False, verbose=0, epochs=128):
     print("🚫 WARNING 🚫: this function will take time to process.")
     print(" ")
     # Set up a scaler
@@ -110,26 +111,118 @@ def k_fold_train(model, data, folds=5):
 
     fold = 1
     for train_index, validate_index in kf.split(data):
-        # Split into train and validate sets
-        train_DF = pd.DataFrame(data.iloc[train_index, :])
-        test_DF = pd.DataFrame(data.iloc[validate_index])
 
-        # Split training set into ys and x's
-        y_train_kfold = train_DF["Outcome"]
-        x_train_kfold = train_DF.drop('Outcome', axis=1)
+        if(is_NN):
+            # Is NN
+            X, y = to_xy(df=data, target="Outcome")
+            y_train_kfold = y[train_index]
+            x_train_kfold = X[train_index]
+            y_test_kfold = y[validate_index]
+            x_test_kfold = X[validate_index]
 
-        # Split validation set into ys and x's
-        y_test_kfold = train_DF["Outcome"]
-        x_test_kfold = train_DF.drop('Outcome', axis=1)
+        else:
+            # Split into train and validate sets
+            train_DF = pd.DataFrame(data.iloc[train_index, :])
+            test_DF = pd.DataFrame(data.iloc[validate_index])
+            # Split training set into ys and x's
+            y_train_kfold = train_DF["Outcome"]
+            x_train_kfold = train_DF.drop('Outcome', axis=1)
+            # Split validation set into ys and x's
+            y_test_kfold = train_DF["Outcome"]
+            x_test_kfold = train_DF.drop('Outcome', axis=1)
 
         # Train model
-        model.fit(x_train_kfold, y_train_kfold)
+        if(is_NN):
+            model.fit(x_train_kfold, y_train_kfold,
+                      verbose=verbose, epochs=epochs)
+        else:
+            model.fit(x_train_kfold, y_train_kfold)
 
-        # Predict
-        print(
-            f"Fold #{fold}, Training Size: {len(train_DF)}, Validation Size: {len(test_DF)}")
-        print(
-            f"Training Score: {model.score(x_train_kfold, y_train_kfold)}")
-        print(f"Testig Score: {model.score(x_test_kfold, y_test_kfold)}")
-        print("\n")
+        # Evaluate
+        if(is_NN):
+            print(f"Fold #{fold} completed")
+            pred = model.predict(x_test_kfold)
+            pred = np.argmax(pred, axis=1)
+            print(f"Prediction\n{pred[:100]}")
+            y_compare = np.argmax(y_test_kfold, axis=1)
+            print(f"Y test\n{y_compare[:100]}")
+            score = metrics.accuracy_score(y_compare, pred)
+            print(f"\nScore is {score}")
+        else:
+            print(
+                f"Fold #{fold}, Training Size: {len(train_DF)}, Validation Size: {len(test_DF)}")
+            print(
+                f"Training Score: {model.score(x_train_kfold, y_train_kfold)}")
+            print(f"Testig Score: {model.score(x_test_kfold, y_test_kfold)}")
+            print("\n")
+
         fold += 1
+
+
+# From ex6Part2 (Lab 6 of Introduction to AI module)
+# Convert a Pandas dataframe to the x,y inputs that TensorFlow needs
+def to_xy(df, target):
+    result = []
+    for x in df.columns:
+        if x != target:
+            result.append(x)
+    # find out the type of the target column.  Is it really this hard? :(
+    target_type = df[target].dtypes
+    target_type = target_type[0] if hasattr(
+        target_type, '__iter__') else target_type
+    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
+    if target_type in (np.int64, np.int32):
+        # Classification
+        dummies = pd.get_dummies(df[target])
+        return df[result].values.astype(np.float32), dummies.values.astype(np.float32)
+    # Regression
+    return df[result].values.astype(np.float32), df[[target]].values.astype(np.float32)
+
+
+# A function that takes the encoded data in, and returns the data "binarified"
+def binarify_from_raw(data, nothing_value=0):
+    # Replace all values in Outcome column, with 0 if "Nothing found - no further action", else 1.
+    # Reference for dictionary idea to replace values: https://stackoverflow.com/questions/17114904/python-pandas-replacing-strings-in-dataframe-with-numbers
+    outcome_splitter = {
+        "Article found - Detailed outcome unavailable": 1,
+        "Local resolution": 1,
+        "Nothing found - no further action": nothing_value,
+        "Offender cautioned": 1,
+        "Offender given drugs possession warning": 1,
+        "Offender given penalty notice": 1,
+        "Suspect arrested": 1,
+        "Suspect summonsed to court": 1
+    }
+    data_binary = data.applymap(lambda s: outcome_splitter.get(
+        s) if s in outcome_splitter else s)
+    return data_binary
+
+
+# A function that takes the encoded data in, and returns the data "binarified"
+def binarify_from_encoded(data, nothing_value=0):
+    # Reference for dictionary idea to replace values: https://stackoverflow.com/questions/17114904/python-pandas-replacing-strings-in-dataframe-with-numbers
+    outcome_splitter = {
+        0: 1,
+        1: 1,
+        2: nothing_value,
+        3: 1,
+        4: 1,
+        5: 1,
+        6: 1,
+        7: 1
+    }
+    data_binary = data.applymap(lambda s: outcome_splitter.get(
+        s) if s in outcome_splitter else s)
+    return data_binary
+
+
+def pie_chart_y(y):
+    labels, frequencies = np.unique(y, return_counts=True)
+    plt.figure(figsize=[20, 5])
+    fig1, ax1 = plt.subplots()
+    ax1.pie(frequencies, labels=labels,
+            autopct='%1.1f%%', shadow=True, startangle=90)
+    # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax1.axis('equal')
+    plt.title("Ratio of Classes")
+    plt.show()
